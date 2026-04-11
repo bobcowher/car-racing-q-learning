567b1a794e1c660f1d0ebf7c6593508f170b62bd
Robert Cowher <bobcowher@gmail.com>
2026-04-11 09:21:37 -0500

    Breaking q learning

--- train.py
+++ train.py
@@ -8,6 +8,6 @@ gym.register_envs(ale_py)
 env = gym.make("ALE/Breakout-v5", render_mode="rgb_array")
 env = LifePenaltyWrapper(env, penalty=-1.0)
 
-agent = Agent(env=env, max_buffer_size=20000)
+agent = Agent(env=env, max_buffer_size=100000)
 
 agent.train(episodes=1200, batch_size=32)
