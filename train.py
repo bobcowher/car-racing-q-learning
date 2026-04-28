09577519b0039819739d06b6022e95230efb57ec
Robert Cowher <bobcowher@gmail.com>
2026-04-27 21:20:50 -0500

    Updated to a batch_size of 64

--- train.py
+++ train.py
@@ -5,4 +5,4 @@ env = gym.make("CarRacing-v3", continuous=False, render_mode="rgb_array")
 
 agent = Agent(env=env, max_buffer_size=100000)
 
-agent.train(episodes=3000, batch_size=32)
+agent.train(episodes=3000, batch_size=64)
