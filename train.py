9ed98c0798f578976173c33ee1121dc0560d8af9
Robert Cowher <bobcowher@gmail.com>
2026-04-28 17:25:06 -0500

    Q learning. 500k buffer

--- train.py
+++ train.py
@@ -3,6 +3,6 @@ import gymnasium as gym
 
 env = gym.make("CarRacing-v3", continuous=False, render_mode="rgb_array")
 
-agent = Agent(env=env, max_buffer_size=100000)
+agent = Agent(env=env, max_buffer_size=500000)
 
 agent.train(episodes=3000, batch_size=64)
